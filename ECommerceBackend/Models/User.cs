e6a6f3f3d8207b3dd44b9165283902a0f794fe07
abdullahestc <54625132+abdullahestc@users.noreply.github.com>
2025-03-06 04:02:03 +0300

    Proje dosyası ekle.

---
+++ ECommerceBackend/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBackendProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
